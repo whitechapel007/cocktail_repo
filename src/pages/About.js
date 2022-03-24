de37308198b6dc6f5de71596d6260f2f83cfdcea
your username <ebukareed@gmail.com>
2022-03-24 10:02:41 +0100

    updated

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore velit,
+        eius nihil vero officia ea magnam sit exercitationem dolore maiores quae
+        ipsam expedita id quod earum provident reiciendis assumenda
+        reprehenderit a quam eum est. Veniam deserunt sint earum eveniet autem
+        id quaerat nobis sunt neque iure fugit hic ipsum reprehenderit
+        laudantium repellendus, modi ullam culpa.
+      </p>
+    </section>
+  );
+};
+
+export default About;
